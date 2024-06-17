37f6f0e8947c5442188e2e193f53a32d012ce7e9
Teo <teosany@icloud.com>
2024-06-18 00:06:03 +0200

    feat: add cache attributes to the index route for improved performance

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,6 +7,7 @@ use App\Repository\CategoryRepository;
 use App\Repository\WineRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\Cache;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
@@ -19,6 +20,7 @@ class MainController extends AbstractController
     {
     }
     #[Route('/', name: 'index')]
+    #[Cache(maxage: 3600, public: true, mustRevalidate: true)]
     public function index(): Response
     {
         $slug = 'red';
